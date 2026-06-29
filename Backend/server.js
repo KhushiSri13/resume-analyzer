1a5f16f51432752ac71dac0083dd8fd5222b9b35
Khushi Srivastava <khushisri1311@gmail.com>
2026-06-29 15:54:12 +0530

    deployment issues fixed

--- Backend/server.js
+++ Backend/server.js
@@ -1,6 +1,7 @@
 require("dotenv").config()
 const app = require("../Backend/src/app")
 const connectToDB = require("../Backend/src/config/database")
+const PORT = process.env.PORT || 3000;
 // const invokeGeminiAi = require("./src/services/ai.services")
 
 // const {resume, selfDescription, jobDescription} = require("./src/services/temp")
@@ -12,7 +13,9 @@ connectToDB()
 // invokeGeminiAi()
 
 // generateInterviewReport(resume, selfDescription, jobDescription)
-
-app.listen(3000, () =>{
-    console.log("server is running")
-})
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
+// app.listen(3000, () =>{
+//     console.log("server is running")
+// })
